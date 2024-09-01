24888f6f1e9b75fa78d787a2fd903f83a8cda713
mathiassellanes <mathias@neocoast.com>
2024-09-01 14:01:07 -0300

    add-drag-and-drop

--- dragHelpers.js
+++ dragHelpers.js
@@ -13,12 +13,41 @@ function dragEnter(e) {
 
 function dragLeave(e) {
   e.stopPropagation();
+  this.style.borderBottom = "";
+  this.style.borderTop = "";
   this.classList.remove('over');
 }
+
+function dragEnd(e) {
+  const listItems = document.querySelectorAll('.draggable');
+  listItems.forEach(function (item) {
+    item.style.borderBottom = "";
+    item.style.borderTop = "";
+    item.classList.remove('over');
+  });
+  this.style.opacity = '1';
+}
+
 
 function dragOver(e) {
   e.preventDefault();
   e.dataTransfer.dropEffect = 'move';
+
+  const bounding = this.getBoundingClientRect();
+  const offset = e.clientY - bounding.top;
+
+  if (offset > bounding.height / 2) {
+    this.style.borderBottom = "2px solid #000";
+    this.style.borderTop = "";
+  } else {
+    this.style.borderTop = "2px solid #000";
+    this.style.borderBottom = "";
+  }
+
+  document.querySelectorAll('.column').forEach(function (column) {
+    column.style.borderTop = "";
+    column.style.borderBottom = "";
+  });
 
   return false;
 }
@@ -27,15 +56,28 @@ function dragDrop(e) {
   e.preventDefault();
   e.stopPropagation();
 
-  this.classList.remove('over');
-}
+  const bounding = this.getBoundingClientRect();
+  const offset = e.clientY - bounding.top;
 
-function dragEnd(e) {
-  const listItens = document.querySelectorAll('.draggable');
-  listItens.forEach(function (item) {
-    item.classList.remove('over');
-  });
-  this.style.opacity = '1';
+  this.style.borderBottom = "";
+  this.style.borderTop = "";
+
+  const draggedCard = dragSrcEl;
+  const targetCard = this;
+
+  const isAfter = offset > bounding.height / 2;
+
+  if (isAfter) {
+    targetCard.insertAdjacentElement('afterend', draggedCard);
+  } else {
+    targetCard.insertAdjacentElement('beforebegin', draggedCard);
+  }
+
+  this.classList.remove('over');
+
+  const targetColumnId = this.closest('.column').id;
+
+  moveCard(draggedCard.id, targetColumnId, targetCard.id, isAfter);
 }
 
 function addEventsDragAndDrop(el) {
@@ -52,8 +94,6 @@ const columnsSelection = document.querySelectorAll('.column');
 const reAddEvents = () => {
   const listItens = document.querySelectorAll('.draggable');
 
-  console.log({ listItens, columnsSelection });
-
   listItens.forEach(function (item) {
     addEventsDragAndDrop(item);
   });
@@ -65,7 +105,6 @@ columnsSelection.forEach(function (column) {
   column.addEventListener('dragover', dragOver, false);
   column.addEventListener('drop', function (e) {
     e.preventDefault();
-    console.log('drop', e);
 
     moveCard(dragSrcEl.id, column.id);
 
