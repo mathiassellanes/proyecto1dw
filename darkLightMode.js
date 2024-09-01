e45e63fb08d1bc1be1a7d05a93971243e379c388
mathiassellanes <mathias@neocoast.com>
2024-09-01 14:41:58 -0300

    add-dark-mode

---
+++ darkLightMode.js
@@ -0,0 +1,34 @@
+const toggleDarkModeBtn = document.getElementById('toggle-dark-mode');
+
+function toggleDarkMode() {
+  document.body.classList.toggle('dark-mode');
+
+  const isDarkMode = document.body.classList.contains('dark-mode');
+
+  localStorage.setItem('darkMode', isDarkMode);
+
+  const img = document.getElementById('toggle-image');
+
+  if (isDarkMode) {
+    img.src = './sun-solid.svg';
+  } else {
+    img.src = './moon-solid.svg';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const darkModePreference = localStorage.getItem('darkMode');
+  if (darkModePreference === 'true') {
+    document.body.classList.add('dark-mode');
+  }
+
+  const img = document.getElementById('toggle-image');
+
+  if (darkModePreference === 'true') {
+    img.src = './sun-solid.svg';
+  } else {
+    img.src = './moon-solid.svg';
+  }
+
+  toggleDarkModeBtn.addEventListener('click', toggleDarkMode);
+});
